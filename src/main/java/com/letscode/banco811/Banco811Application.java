8100f175deafcca5457531d4c22444736e81fd3d
Mahhh <73047662+marisasales@users.noreply.github.com>
2022-04-07 03:50:20 -0300

    Initial commit

---
+++ src/main/java/com/letscode/banco811/Banco811Application.java
@@ -0,0 +1,13 @@
+package com.letscode.banco811;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Banco811Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Banco811Application.class, args);
+	}
+
+}
