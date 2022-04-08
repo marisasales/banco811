008fa1cce04c06ea7a7ffe799211b8118bda97af
Mahhh <73047662+marisasales@users.noreply.github.com>
2022-04-08 18:04:28 -0300

    refactor: enable jpa auditing

--- src/main/java/com/letscode/banco811/Banco811Application.java
+++ src/main/java/com/letscode/banco811/Banco811Application.java
@@ -2,8 +2,10 @@ package com.letscode.banco811;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Banco811Application {
 
 	public static void main(String[] args) {
