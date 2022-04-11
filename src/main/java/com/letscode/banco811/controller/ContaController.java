825d0bed628a7dde4f00c4dbf2ba603f71084a7f
Mahhh <73047662+marisasales@users.noreply.github.com>
2022-04-11 14:27:51 -0300

    refactor: update getById | feat: add getAllViewByTipoConta

--- src/main/java/com/letscode/banco811/controller/ContaController.java
+++ src/main/java/com/letscode/banco811/controller/ContaController.java
@@ -2,10 +2,14 @@ package com.letscode.banco811.controller;
 
 import com.letscode.banco811.dto.ContaRequest;
 import com.letscode.banco811.dto.ContaResponse;
+import com.letscode.banco811.model.TipoConta;
+import com.letscode.banco811.projection.ContaView;
 import com.letscode.banco811.service.ContaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/conta")
@@ -28,7 +32,12 @@ public class ContaController {
 
     @GetMapping("/{id}")
     public ContaResponse getById(@PathVariable Integer id) {
-        return contaService.getById(id);
+        return new ContaResponse(contaService.getById(id));
+    }
+
+    @GetMapping("/view")
+    public List<ContaView> getAllViewByTipoConta(@RequestParam TipoConta tipoConta) {
+        return contaService.getAllViewByTipoConta(tipoConta);
     }
 
     @PutMapping("/{id}")
