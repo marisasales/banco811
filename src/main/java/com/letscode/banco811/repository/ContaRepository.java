2419c9dfcf3934c37b81257a0efdb2c8a7a3ae26
Mahhh <73047662+marisasales@users.noreply.github.com>
2022-04-11 13:49:57 -0300

    feat: add findAllByTipoConta and findByNumeroAndAgencia

--- src/main/java/com/letscode/banco811/repository/ContaRepository.java
+++ src/main/java/com/letscode/banco811/repository/ContaRepository.java
@@ -1,9 +1,20 @@
 package com.letscode.banco811.repository;
 
 import com.letscode.banco811.model.Conta;
+import com.letscode.banco811.model.TipoConta;
+import com.letscode.banco811.projection.ContaView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ContaRepository extends JpaRepository<Conta, Integer> {
+
+    List<ContaView> findAllByTipoConta(TipoConta tipoConta);
+
+    @Query("select c from Conta c where c.numero = :numero and c.agencia = :agencia")
+    Conta findByNumeroAndAgencia(@Param("numero") Integer numero, @Param("agencia") Integer agencia);
 }
