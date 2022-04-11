f7fd45cf90d90b045998892e0340647322a3d817
Mahhh <73047662+marisasales@users.noreply.github.com>
2022-04-11 14:27:27 -0300

    feat: add transacoes list and Conta constructor

--- src/main/java/com/letscode/banco811/model/Conta.java
+++ src/main/java/com/letscode/banco811/model/Conta.java
@@ -1,5 +1,6 @@
 package com.letscode.banco811.model;
 
+import com.letscode.banco811.dto.ContaRequest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Table(name = "conta")
 @Entity
@@ -46,4 +48,14 @@ public class Conta {
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "usuario_id", referencedColumnName = "id")
     private Usuario usuario;
+
+    @OneToMany(mappedBy = "conta", cascade = CascadeType.ALL)
+    private List<Transacao> transacoes;
+
+    public Conta(ContaRequest contaRequest) {
+        this.numero = contaRequest.getNumero();
+        this.agencia = contaRequest.getAgencia();
+        this.saldo = contaRequest.getSaldo();
+        this.tipoConta = contaRequest.getTipoConta();
+    }
 }
